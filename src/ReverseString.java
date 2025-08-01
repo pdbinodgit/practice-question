f74117b89099c41a4f6eeb9140a93d418ab02eb4
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-01 20:46:37 +0545

    code for reverse string

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -11,5 +11,18 @@ public class ReverseString {
         }
         System.out.println("Original "+name);
         System.out.println("reverse "+reverse);
+
+
+        /*
+        Using array
+         */
+        char[] arrayChar=name.toCharArray();
+        System.out.println("orginal ");
+        System.out.println("array size is"+arrayChar.length);
+        reverse="";
+        for (int i=arrayChar.length-1;i>=0;i--){
+            reverse=reverse+arrayChar[i];
+        }
+        System.out.println("reverse "+reverse);
     }
 }
