8c6ff83a5abb2ebdc424d730c2ad9f89bc6a801a
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-15 21:53:53 +0545

    array example

---
+++ src/array/ArrayExample.java
@@ -0,0 +1,15 @@
+package array;
+
+public class ArrayExample {
+
+    public static void main(String[] args) {
+        /*
+        * array is a collection same data type element.
+        *
+        *
+        * */
+
+        int[] arr={1,2,4,4,5};
+        System.out.println(arr[1]);
+    }
+}
