be2956a87c2e33b56fa1062d42901a486c2e243f
Bishnu Prasad Pandey <pandeybinod2075@gmail.com>
2025-08-16 22:02:23 +0545

    sort array

--- src/array/SortArray.java
+++ src/array/SortArray.java
@@ -18,8 +18,26 @@ public class SortArray {
             }
 
         }
+        System.out.println("array sort ascending order");
         for (int i=0;i<arr.length;i++){
             System.out.println(arr[i]);
         }
+        System.out.println("array sort descending order");
+        for (int i=0;i<n;i++){
+            for (int j=i+1;j<n;j++){
+                if (arr[i]<arr[j]){
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+
+        }
+        for (int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+
+
+
     }
 }
